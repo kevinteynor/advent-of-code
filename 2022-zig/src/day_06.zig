15b76697f4fd4d3fbdcf098b02a2ac631200a7ba
Kevin Teynor <kevinteynor@gmail.com>
2022-12-08 16:25:09 -0500

    added day_06 (part 2)

--- 2022-zig/src/day_06.zig
+++ 2022-zig/src/day_06.zig
@@ -9,46 +9,18 @@ pub fn run(input: std.fs.File) !void {
     var buffered = std.io.bufferedReader(input.reader());
     var reader = buffered.reader();
 
-    const sopOffset = findSOPMarker(reader).?;
+    // part 1
+    const sopOffset = findFirstUniqueRangeEnd(4, reader).?;
     try common.printLnFmt("SOP marker index: {}", .{sopOffset});
+
+    try input.seekTo(0);
+    buffered = std.io.bufferedReader(input.reader());
+    reader = buffered.reader();
+
+    // part 2
+    const messageOffset = findFirstUniqueRangeEnd(14, reader).?;
+    try common.printLnFmt("message marker index: {}", .{messageOffset});
 }
-
-fn findSOPMarker(reader: anytype) ?usize {
-
-    var buf: [4]u8 = undefined;
-
-    buf[0] = reader.readByte() catch return null;
-    buf[1] = reader.readByte() catch return null;
-    buf[2] = reader.readByte() catch return null;
-    buf[3] = reader.readByte() catch return null;
-    var idx: usize = 4;
-    var window = buf[0..];
-
-    while (!isUnique(u8,  window)) {
-        buf[idx % 4] = reader.readByte() catch return null;
-        if (buf[idx % 4] == '\n') {
-            return null;
-        }
-
-        idx += 1;
-    }
-
-    return idx;
-}
-
-test "Find SOP Marker" {
-    var input = std.io.fixedBufferStream("mjqjpqmgbljsphdztnvjfqwrcgsmlb");
-    try expectEqual(@as(usize, 7), findSOPMarker(input.reader()).?);
-    input = std.io.fixedBufferStream("bvwbjplbgvbhsrlpgdmjqwftvncz");
-    try expectEqual(@as(usize, 5), findSOPMarker(input.reader()).?);
-    input = std.io.fixedBufferStream("nppdvjthqldpwncqszvftbrmjlhg");
-    try expectEqual(@as(usize, 6), findSOPMarker(input.reader()).?);
-    input = std.io.fixedBufferStream("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg");
-    try expectEqual(@as(usize, 10), findSOPMarker(input.reader()).?);
-    input = std.io.fixedBufferStream("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw");
-    try expectEqual(@as(usize, 11), findSOPMarker(input.reader()).?);
-}
-
 
 fn isUnique(comptime T: type, slice: []const T) bool {
     if (slice.len == 0) {
@@ -75,4 +47,54 @@ test "Is Unique" {
     try expect(!isUnique(u8, "AA"));
     try expect(isUnique(u8, "A"));
     try expect(isUnique(u8, ""));
+}
+
+fn findFirstUniqueRangeEnd(comptime N: usize, reader: anytype) ?usize {
+
+    var buf: [N]u8 = undefined;
+
+    for (buf) |*i| {
+        i.* = reader.readByte() catch return null;
+    }
+
+    var idx: usize = N;
+    var window = buf[0..];
+    var index: i32 = @intCast(i32,N);
+
+    while (!isUnique(u8,  window)) {
+        buf[idx % N] = reader.readByte() catch return null;
+        if (buf[idx % N] == '\n') {
+            return null;
+        }
+
+        idx += 1;
+        index += 1;
+    }
+
+    return idx;
+}
+
+test "Find First Unique Range End" {
+    var input = std.io.fixedBufferStream("mjqjpqmgbljsphdztnvjfqwrcgsmlb");
+    try expectEqual(@as(usize, 7), findFirstUniqueRangeEnd(4, input.reader()).?);
+    input = std.io.fixedBufferStream("bvwbjplbgvbhsrlpgdmjqwftvncz");
+    try expectEqual(@as(usize, 5), findFirstUniqueRangeEnd(4, input.reader()).?);
+    input = std.io.fixedBufferStream("nppdvjthqldpwncqszvftbrmjlhg");
+    try expectEqual(@as(usize, 6), findFirstUniqueRangeEnd(4, input.reader()).?);
+    input = std.io.fixedBufferStream("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg");
+    try expectEqual(@as(usize, 10), findFirstUniqueRangeEnd(4, input.reader()).?);
+    input = std.io.fixedBufferStream("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw");
+    try expectEqual(@as(usize, 11), findFirstUniqueRangeEnd(4, input.reader()).?);
+
+
+    input = std.io.fixedBufferStream("mjqjpqmgbljsphdztnvjfqwrcgsmlb");
+    try expectEqual(@as(usize, 19), findFirstUniqueRangeEnd(14, input.reader()).?);
+    input = std.io.fixedBufferStream("bvwbjplbgvbhsrlpgdmjqwftvncz");
+    try expectEqual(@as(usize, 23), findFirstUniqueRangeEnd(14, input.reader()).?);
+    input = std.io.fixedBufferStream("nppdvjthqldpwncqszvftbrmjlhg");
+    try expectEqual(@as(usize, 23), findFirstUniqueRangeEnd(14, input.reader()).?);
+    input = std.io.fixedBufferStream("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg");
+    try expectEqual(@as(usize, 29), findFirstUniqueRangeEnd(14, input.reader()).?);
+    input = std.io.fixedBufferStream("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw");
+    try expectEqual(@as(usize, 26), findFirstUniqueRangeEnd(14, input.reader()).?);
 }
