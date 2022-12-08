8d8a87b06e9e9355c57a080601af5c42dcee3f57
Kevin Teynor <kevinteynor@gmail.com>
2022-12-08 13:22:33 -0500

    added readLinesUntilBlank utility that reads all lines from a stream until an empty line is found

--- 2022-zig/src/common.zig
+++ 2022-zig/src/common.zig
@@ -90,3 +90,50 @@ test "read N lines" {
     const third = try readNLines(2, reader, &buffer);
     try expect(third == null);
 }
+
+pub fn readLinesUntilBlank(reader: anytype, buffer: []u8, allocator: std.mem.Allocator) !std.ArrayList([]u8) {
+    var lines = std.ArrayList([]u8).init(allocator);
+    var offset: usize = 0;
+    // var count: usize = 0;
+    while (true) {
+        const line = try readLine(reader, buffer[offset..]) orelse break;
+
+        // check for empty line
+        if (line.len == 0) break;
+        if (line.len == 1 and line[0] == '\n') break;
+        if (line.len == 2 and line[0] == '\r' and line[1] == '\n') break;
+
+        try lines.append(line);
+        offset += line.len;
+        // count += 1;
+    }
+    return lines;
+}
+
+test "read lines until blank" {
+    var src = std.io.fixedBufferStream(
+        \\one
+        \\two
+        \\three
+        \\
+        \\four
+        \\five
+    );
+    const reader = src.reader();
+    var buffer: [128]u8 = undefined;
+
+    const first = try readLinesUntilBlank(reader, &buffer, std.testing.allocator);
+    defer first.deinit();
+    try expectEqualStrings("one", first.items[0]);
+    try expectEqualStrings("two", first.items[1]);
+    try expectEqualStrings("three", first.items[2]);
+
+    const second = try readLinesUntilBlank(reader, &buffer, std.testing.allocator);
+    defer second.deinit();
+    try expectEqualStrings("four", second.items[0]);
+    try expectEqualStrings("five", second.items[1]);
+    
+    const third = try readLinesUntilBlank(reader, &buffer, std.testing.allocator);
+    defer third.deinit();
+    try expect(third.items.len == 0);
+}
