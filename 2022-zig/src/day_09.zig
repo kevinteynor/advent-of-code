0f6551e044e350f5347e271f209f8270cc432e65
Kevin Teynor <kevinteynor@gmail.com>
2022-12-11 18:15:27 -0500

    added day_09 (part 2). slight refactor to original method to make it work for both parts.

--- 2022-zig/src/day_09.zig
+++ 2022-zig/src/day_09.zig
@@ -7,103 +7,106 @@ const common = @import("common.zig");
 
 pub fn run(input: std.fs.File, allocator: Allocator) !void {
     try common.printLn("Day 9: Rope Bridge");
-    
+
     // part 1
     var buffered = std.io.bufferedReader(input.reader());
     var reader = buffered.reader();
-    var count = try simulateRope(reader, allocator);
-    try common.printLnFmt("Unique tail positions count: {}", .{count});
+    var count = try simulateRope(2, reader, allocator);
+    try common.printLnFmt("Unique tail positions (2 knots) count: {}", .{count});
+
+    // part 2
+    try input.seekTo(0);
+    buffered = std.io.bufferedReader(input.reader());
+    reader = buffered.reader();
+    var c2 = try simulateRope(10, reader, allocator);
+    try common.printLnFmt("Unique tail positions (10 knots) count: {}", .{c2});
+
 }
 
 const Coordinate = [2]isize;
 
-fn absOffset(from: Coordinate, to: Coordinate) Coordinate {
-    return .{
-        std.math.absInt(from[0] - to[0]) catch 0,
-        std.math.absInt(from[1] - to[1]) catch 0,
+fn RopeState(comptime length: usize) type {
+    return struct {
+        const Self = @This();
+        const History = std.AutoHashMap([2]isize, void);
+
+        knots: [length]Coordinate,
+
+        history: History,
+
+        fn init(allocator: Allocator) !Self {
+            var self = Self{
+                .knots = [_]Coordinate{Coordinate{ 0, 0 }} ** length,
+                .history = History.init(allocator),
+            };
+            try self.history.put(Coordinate{ 0, 0 }, {});
+            return self;
+        }
+
+        fn deinit(self: *Self) void {
+            self.history.deinit();
+        }
+
+        fn move(self: *Self, dx: i2, dy: i2) !void {
+
+            var k: usize = 0;
+            self.knots[k][0] += dx;
+            self.knots[k][1] += dy;
+            k += 1;
+
+            // pull knots towards previous
+            while (k < self.knots.len) : (k += 1) {
+                var offset = Coordinate{ self.knots[k - 1][0] - self.knots[k][0], self.knots[k - 1][1] - self.knots[k][1] };
+                if (try std.math.absInt(offset[0]) > 1 or try std.math.absInt(offset[1]) > 1) {
+                    offset[0] = std.math.clamp(offset[0], -1, 1);
+                    offset[1] = std.math.clamp(offset[1], -1, 1);
+                    self.knots[k][0] += offset[0];
+                    self.knots[k][1] += offset[1];
+                }
+            }
+
+            // store history of tail
+            try self.history.put(self.knots[self.knots.len - 1], {});
+        }
+
+        fn historyCount(self: *const Self) usize {
+            return self.history.count();
+        }
     };
 }
 
-test "Coordinate offset" {
-    const a = Coordinate{ 1, 1 };
-    var b = absOffset(a, .{ 2, 2 });
-    try expectEqual(Coordinate{ 1, 1 }, b);
-    b = absOffset(a, .{ 0, 0 });
-    try expectEqual(Coordinate{ 1, 1 }, b);
-    b = absOffset(a, .{ 10, -6 });
-    try expectEqual(Coordinate{ 9, 7 }, b);
-}
-
-const RopeState = struct {
-    const Self = @This();
-    const History = std.AutoHashMap([2]isize, void);
-
-    head: Coordinate,
-    tail: Coordinate,
-    history: History,
-
-    fn init(allocator: Allocator) !Self {
-        var self = Self{
-            .head = Coordinate{ 0, 0 },
-            .tail = Coordinate{ 0, 0 },
-            .history = History.init(allocator),
-        };
-        try self.history.put(self.tail, {});
-        return self;
-    }
-
-    fn deinit(self: *Self) void {
-        self.history.deinit();
-    }
-
-    fn move(self: *Self, dx: i2, dy: i2) !void {
-        const new_head = .{self.head[0] + dx, self.head[1] + dy};
-        const offset = absOffset(self.tail, new_head);
-        if (offset[0] > 1 or offset[1] > 1) {
-            self.tail = self.head;
-            try self.history.put(self.tail, {});
-        }
-        self.head = new_head;
-    }
-
-    fn historyCount(self: *const Self) usize {
-        return self.history.count();
-    }
-};
-
-
 test "Rope State" {
-    var rope = try RopeState.init(std.testing.allocator);
+    var rope = try RopeState(2).init(std.testing.allocator);
     defer rope.deinit();
 
-    try expectEqual(Coordinate{ 0, 0 }, rope.head);
-    try expectEqual(Coordinate{ 0, 0 }, rope.tail);
+    try expectEqual(Coordinate{ 0, 0 }, rope.knots[0]);
+    try expectEqual(Coordinate{ 0, 0 }, rope.knots[1]);
 
     try rope.move(1, 0); // H: 1,0
     try rope.move(1, 0); // H: 2,0
     try rope.move(1, 0); // H: 3,0
-    try expectEqual(Coordinate{ 2, 0 }, rope.tail);
+    try expectEqual(Coordinate{ 2, 0 }, rope.knots[1]);
 
     try rope.move(0, 1); // H: 3,1
-    try expectEqual(Coordinate{ 2, 0 }, rope.tail);
+    try expectEqual(Coordinate{ 2, 0 }, rope.knots[1]);
 
     try rope.move(0, 1); // H: 3,2
-    try expectEqual(Coordinate{ 3, 1 }, rope.tail);
+    try expectEqual(Coordinate{ 3, 1 }, rope.knots[1]);
 
     try rope.move(0, -1); // H: 3,1
-    try expectEqual(Coordinate{ 3, 1 }, rope.tail);
+    try expectEqual(Coordinate{ 3, 1 }, rope.knots[1]);
 
     try rope.move(-1, 0); // H: 2,1
     try rope.move(-1, 0); // H: 1,1
-    try expectEqual(Coordinate{ 2, 1 }, rope.tail);
+    try expectEqual(Coordinate{ 2, 1 }, rope.knots[1]);
 
     try expectEqual(@as(usize, 5), rope.historyCount());
 }
 
-fn simulateRope(reader: anytype, allocator: Allocator) !usize {
-    var rope = try RopeState.init(allocator);
+fn simulateRope(comptime length: usize, reader: anytype, allocator: Allocator) !usize {
+    var rope = try RopeState(length).init(allocator);
     defer rope.deinit();
-    
+
     var buffer: [8]u8 = undefined;
     while (try common.readLine(reader, &buffer)) |line| {
         var count = try std.fmt.parseInt(usize, line[2..], 10);
@@ -137,6 +140,8 @@ test "Rope Simulation" {
         \\R 2
     );
     var reader = input.reader();
-    const count = try simulateRope(reader, std.testing.allocator);
-    try expectEqual(@as(usize, 13), count);
+    try expectEqual(@as(usize, 13), try simulateRope(2, reader, std.testing.allocator));
+
+    reader = input.reader();
+    try expectEqual(@as(usize, 1), try simulateRope(10, reader, std.testing.allocator));
 }
